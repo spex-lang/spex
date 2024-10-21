513e365542f1598c9c2fd6e16079a6562e8570bd
Stevan Andjelkovic <stevana@users.noreply.github.com>
2024-10-21 15:36:34 +0200

    ci: improve dockerfile for static build

--- release/entrypoint.sh
+++ release/entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+# To avoid cabal update always refetching the latest packages, we pin it to a
+# specific date. This can be removed once the following bug is fixed:
+# https://github.com/haskell/cabal/issues/8572
+
 cd /mnt \
-    && cabal update \
-    && cabal build exe:spex exe:spex-demo-petstore --enable-executable-static 
+    && cabal update hackage.haskell.org,2024-10-17T07:25:36Z \
+    && cabal build exe:spex exe:spex-demo-petstore --enable-executable-static
+
+    # We can't use cabal install here, because that causes --version to break
+    # due to the following bug: https://github.com/acfoltzer/gitrev/issues/23
+    #
+    # && cabal install exe:spex exe:spex-demo-petstore \
+    #        --enable-executable-static \
+    #        --install-method=copy \
+    #        --overwrite-policy=always \
+    #        --installdir=/mnt/bin
