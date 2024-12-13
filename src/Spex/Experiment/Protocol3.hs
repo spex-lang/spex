ce71eab4f284f987dbfc202304d72246eaac4b09
Stevan Andjelkovic <stevana@users.noreply.github.com>
2024-12-13 12:08:09 +0100

    feat: fix some unhappy paths for 2PC

--- src/Spex/Experiment/Protocol3.hs
+++ src/Spex/Experiment/Protocol3.hs
@@ -5,9 +5,7 @@ import Control.Exception
 import Data.IORef
 import Data.Map.Strict (Map)
 import Data.Map.Strict qualified as Map
-import Data.Typeable
 import System.Random
-import Text.Read (readEither)
 
 ------------------------------------------------------------------------
 
@@ -30,8 +28,8 @@ data Transition state node msg
   = Transition (state -> Bool) (Session state node msg)
 
 data Session state node msg
-  = Send node msg (Session state node msg)
-  | Recv node (msg -> Session state node msg)
+  = Send [node] msg (Session state node msg)
+  | Recv [node] (msg -> Session state node msg)
   | Or (Session state node msg) (Session state node msg)
   | End state
 
@@ -39,16 +37,24 @@ data SomeProtocol node msg
   = forall state.
     SomeProtocol (Protocol state node msg) state
 
-data TMState = TMInit | TMWorking | TMCommitted
+data TMState = TMInit | TMWorking | TMCommitted | TMAborted
   deriving (Eq, Show)
 
-data RMState = RMInit | RMPrepared
+data RMState = RMInit | RMPrepared | RMAborted
   deriving (Eq, Show)
 
 data ClientState = ClientInit
   deriving (Eq, Show)
 
-data Msg = Write | Prepare | Prepared | Commit | Comitted | Ack
+data Msg
+  = Write
+  | Prepare
+  | Prepared
+  | Commit
+  | Comitted
+  | Abort
+  | Aborted
+  | Ack
   deriving (Eq, Show)
 
 tmProtocol :: Protocol TMState Node Msg
@@ -57,53 +63,38 @@ tmProtocol =
     [ Transition
         (== TMInit)
         ( Recv
-            Client
-            (\Write -> Send RM1 Prepare (Send RM2 Prepare (End TMWorking)))
+            [Client]
+            (\Write -> Send [RM1, RM2] Prepare (End TMWorking))
         )
     , Transition
         (== TMWorking)
         ( Recv
-            RM1
+            [RM1, RM2]
             ( \Prepared ->
                 Recv
-                  RM2
-                  (\Prepared -> Send RM1 Commit (Send RM2 Commit (End TMCommitted)))
+                  [RM1, RM2]
+                  (\Prepared -> Send [RM1, RM2] Commit (End TMCommitted))
             )
         )
     , Transition
         (== TMCommitted)
-        ( Recv RM1 (\Ack -> Recv RM2 (\Ack -> Send Client Ack (End TMInit)))
+        ( Recv
+            [RM1, RM2]
+            (\Ack -> Recv [RM1, RM2] (\Ack -> Send [Client] Ack (End TMInit)))
+        )
+    , Transition
+        (== TMWorking)
+        ( Recv
+            [RM1, RM2]
+            (\Aborted -> Send [RM1, RM2, Client] Abort (End TMAborted))
+        )
+    , Transition
+        (== TMCommitted)
+        ( Recv
+            [RM1, RM2]
+            (\Ack -> Recv [RM1, RM2] (\Ack -> Send [Client] Ack (End TMInit)))
         )
     ]
-
--- , Rule
---    (== Working)
---    (\_s -> Recv RM1 (\Abort -> Send RM2 Abort (End Aborted)))
--- , -- XXX: symmetric...
---  Rule
---    (== Working)
---    (\_s -> Recv RM2 (\Abort -> Send RM1 Abort (End Aborted)))
--- , -- XXX: Symmetric to the above
---  Rule
---    (== Working)
---    ( \_s ->
---        Recv
---          RM2
---          ( \Prepare ->
---              Recv
---                RM1
---                (\Prepare -> Send RM1 Commit (Send RM2 Commit (End Committed)))
---          )
---    )
--- , Rule
---    (== Committed)
---    ( \_s -> Recv RM1 (\Ack -> Recv RM2 (\Ack -> Send Client Ack (End Init)))
---    )
--- , -- XXX: symmetric
---  Rule
---    (== Committed)
---    ( \_s -> Recv RM2 (\Ack -> Recv RM1 (\Ack -> Send Client Ack (End Init)))
---    )
 
 rmProtocol :: Protocol RMState Node Msg
 rmProtocol =
@@ -111,23 +102,28 @@ rmProtocol =
     [ Transition
         (== RMInit)
         ( Recv
-            TM
+            [TM]
             ( \Prepare ->
-                Send TM Prepared (End RMPrepared)
-                -- `Or` Send TM Abort (End Aborted)
+                Send [TM] Prepared (End RMPrepared)
+                  `Or` Send [TM] Aborted (End RMAborted)
             )
         )
     , Transition
         (== RMPrepared)
-        (Recv TM (\Commit -> Send TM Ack (End RMInit)))
+        (Recv [TM] (\Commit -> Send [TM] Ack (End RMInit)))
+    , Transition (== RMPrepared) (Recv [TM] (\Abort -> End RMAborted))
+    , Transition (== RMAborted) (Recv [TM] (\Abort -> End RMAborted))
     ]
-
--- , Rule (== Prepared) (\_s -> Recv TM (\Abort -> End Aborted))
--- , Rule (== Aborted) (\_s -> Recv TM (\Abort -> End Aborted))
 
 clientProtocol :: Protocol ClientState Node Msg
 clientProtocol =
-  Protocol [Transition (== ClientInit) (Recv TM (\Ack -> End ClientInit))]
+  Protocol
+    [ Transition
+        (== ClientInit)
+        ( Recv [TM] (\Ack -> End ClientInit)
+            `Or` Recv [TM] (\Abort -> End ClientInit)
+        )
+    ]
 
 data Deployment node msg = Deployment
   { topology :: Topology node -- NOTE: not used yet...
@@ -157,26 +153,41 @@ newMock ::
 newMock protocol state = Mock protocol (Right state)
 
 runMock ::
-  (Eq node) =>
+  forall state node msg.
+  (Eq node, Show node) =>
   Mock state node msg
   -> Envelope node msg
+  -> StdGen
   -> (Mock state node msg, [(node, msg)])
-runMock mock envelope = case mock.state of
-  Left (Recv node k)
-    | node /= envelope.from -> error "runMock: node mismatch"
-    | otherwise -> go [] (k envelope.content)
+runMock mock envelope stdGen = case mock.state of
+  Left session -> runSession session stdGen []
   Right state -> case lookupState state mock.protocol of
     [] -> (mock, [])
-    [Recv node k]
-      | node /= envelope.from -> error "runMock: node mismatch"
-      | otherwise -> go [] (k envelope.content)
-    [Send node msg session'] -> go [(node, msg)] session'
-    _ : _ : _ -> error "runMock: more than one session"
+    [session] -> runSession session stdGen []
+    sessions ->
+      let (index, stdGen') = randomR (0, length sessions - 1) stdGen
+      in  runSession (sessions !! index) stdGen' []
   where
-    go sentMsgs (Send toNode msg session') = go ((toNode, msg) : sentMsgs) session'
-    go sentMsgs (Or session session') = error "XXX: or"
-    go sentMsgs (End state') = (mock {state = Right state'}, reverse sentMsgs)
-    go sentMsgs session = (mock {state = Left session}, reverse sentMsgs)
+    runSession ::
+      Session state node msg
+      -> StdGen
+      -> [(node, msg)]
+      -> (Mock state node msg, [(node, msg)])
+    runSession (Recv nodes k) stdGen' sentMsgs
+      | envelope.from `notElem` nodes =
+          error $
+            "runMock: node mismatch, "
+              ++ show envelope.from
+              ++ " `notElem` "
+              ++ show nodes
+      | otherwise = runSession (k envelope.content) stdGen' sentMsgs
+    runSession (Send nodes msg session') stdGen' sentMsgs = runSession session' stdGen' (sentMsgs ++ zip nodes (repeat msg))
+    runSession (Or session session') stdGen' sentMsgs =
+      let (bool, stdGen'') = random stdGen'
+      in  if bool
+            then runSession session stdGen'' sentMsgs
+            else runSession session' stdGen'' sentMsgs
+    runSession (End state') _stdGen' sentMsgs = (mock {state = Right state'}, sentMsgs)
 
 lookupState ::
   state
@@ -185,12 +196,14 @@ lookupState ::
 lookupState state (Protocol transitions) = go transitions
   where
     go [] = []
-    go (Transition pred session : transitions')
-      | pred state = session : go transitions'
+    go (Transition precondition session : transitions')
+      | precondition state = session : go transitions'
       | otherwise = go transitions'
 
+type role SomeMock representational representational
 data SomeMock node msg = forall state. SomeMock (Mock state node msg)
 
+type role Sim nominal representational
 data Sim node msg = Sim
   { nodes :: Map node (SomeMock node msg)
   , messages :: [Envelope node msg]
@@ -198,6 +211,7 @@ data Sim node msg = Sim
   , trace :: [Envelope node msg]
   }
 
+type role Envelope representational representational
 data Envelope node msg = Envelope
   { from :: node
   , to :: node
@@ -229,34 +243,39 @@ newSim deployment initialMessages seed = do
       (node, SomeMock (newMock protocol initialState)) : makeMocks rest
 
 stepSim ::
-  (Ord node) => Sim node msg -> Either (Sim node msg) (Sim node msg)
+  (Ord node, Show node) =>
+  Sim node msg
+  -> Either (Sim node msg) (Sim node msg)
 stepSim sim = case sim.messages of
   [] -> Right sim
   envelope : envelopes -> case sim.nodes Map.! envelope.to of
     SomeMock mock ->
-      let (mock', sentMsgs) = runMock mock envelope
+      let (stdGen', stdGen'') = split sim.prng
+          (mock', sentMsgs) = runMock mock envelope stdGen'
       in  Left
             Sim
               { nodes = Map.insert envelope.to (SomeMock mock') sim.nodes
               , messages = envelopes ++ map (uncurry (Envelope envelope.to)) sentMsgs
-              , prng = sim.prng
+              , prng = stdGen''
               , trace = envelope : sim.trace
               }
 
 runSim ::
-  (Ord node) => Sim node msg -> [Envelope node msg]
+  (Ord node, Show node) => Sim node msg -> [Envelope node msg]
 runSim sim = case stepSim sim of
   Right sim' -> reverse sim'.trace
   Left sim' -> runSim sim'
 
 test :: IO ()
 test = do
+  let seed = 123
   sim <-
     newSim
       twoPCDeployment
       [Envelope {from = Client, to = TM, content = Write}]
-      123
-  assert (runSim sim == trace) (return ())
+      seed
+  -- mapM_ print (runSim sim)
+  assert (runSim sim == unhappy) (return ())
   where
     trace =
       [ Envelope {from = Client, to = TM, content = Write}
@@ -269,4 +288,15 @@ test = do
       , Envelope {from = RM1, to = TM, content = Ack}
       , Envelope {from = RM2, to = TM, content = Ack}
       , Envelope {from = TM, to = Client, content = Ack}
+      ]
+
+    unhappy =
+      [ Envelope {from = Client, to = TM, content = Write}
+      , Envelope {from = TM, to = RM1, content = Prepare}
+      , Envelope {from = TM, to = RM2, content = Prepare}
+      , Envelope {from = RM1, to = TM, content = Aborted}
+      , Envelope {from = RM2, to = TM, content = Aborted}
+      , Envelope {from = TM, to = RM1, content = Abort}
+      , Envelope {from = TM, to = RM2, content = Abort}
+      , Envelope {from = TM, to = Client, content = Abort}
       ]
